eb782f809aad040b2fb96888e127f884a2f416e9
Boxor <cdschonlau@hotmail.com>
2019-04-11 15:07:00 -0500

    Pushing what I have so far because Grayton asked me to

--- calc.js
+++ calc.js
@@ -1,5 +1,52 @@
-var one = document.querySelector("#1");
+var currentNum = "";
+var equation = [];
 
-one.addEventListener("click", function(){
-    
+var nums = document.querySelectorAll(".num");
+
+var operators = document.querySelectorAll(".oper");
+
+var equals = document.querySelector("#equals");
+
+nums.forEach(function(val){
+    val.addEventListener("click", function(){
+
+//        if(equation.length > 2){
+//            mathCalc(equation);
+//        }
+//
+//        else{
+            currentNum = currentNum + this.value;
+            console.log(currentNum);
+//        }
+    });
+});
+
+//var operator = document.querySelectorAll(".oper");
+//operator.forEach(function(){
+//    operator.addEventListener("click", function(){
+//        currentNum = parseInt(currentNum);
+//        console.log(operator);
+//    });
+//});
+
+operators.forEach(function(val){
+    val.addEventListener("click", function(){
+        var intNum = parseInt(currentNum);
+        equation.push(intNum);
+        
+        equation.push(this.value);
+        currentNum = "";
+        console.log(equation);
+    });
+});
+
+//function mathCalc(equation){
+//    var newNum = equation[0] + equation[1] + equation[2];
+//    console.log(newNum);
+//}
+
+equals.addEventListener("click", function(){
+    equation.toString();
+    console.log(equation);
+//    equation = "";
 });
